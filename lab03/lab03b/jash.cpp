4a9f3150b44f96855399e5849bab241d7178d7d5
Anurag Shirolkar <ashirolkar47@gmail.com>
2015-01-30 15:16:34 +0530

    added parsing for input output redirection

---
+++ lab03/lab03b/jash.cpp
@@ -0,0 +1,313 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <wait.h>
+#include <sstream>
+#include <string.h>
+using namespace std;
+
+#define MAXLINE 1000
+#define DEBUG 1
+
+/* Function declarations and globals */
+int parent_pid ;
+vector<string> tokenize(string);
+int execute_command(vector<string>) ;
+
+// Helper function for function below
+vector<string> &split(const string &s, char delim, vector<string> &elems) {
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+/**
+ * splits the string with delimiter and returns a vector of strings
+ */
+vector<vector<string> > split(vector<string> & command) {
+	vector<vector<string> > commands;
+	vector<string> current_command;
+	for (int i = 1; i < command.size(); i++) {
+		if (command[i] == ":::") {
+			commands.push_back(current_command);
+			current_command.clear();
+		}
+		else {
+			current_command.push_back(command[i]);
+		}
+	}
+	if (current_command.size()) commands.push_back(current_command);
+	return commands;
+}
+
+void deallocated(const char** pointers, int size){
+	for (int i=0; i< size; i++){
+		delete[] pointers[i];
+	}
+	delete[] pointers;
+}
+
+// Handles SIGINT and SIGQUIT
+void handler(int dummy){
+	cin.clear();
+
+	if (getpid() != parent_pid){
+		exit(0);
+	}
+}
+
+int main()
+{
+	// assigning handler functions
+	signal(SIGINT, handler);
+	signal(SIGQUIT, handler);
+
+	parent_pid = getpid();
+	//cout<<parent_pid<<endl;
+	string input;
+	vector<string> history;
+	while(1) {
+		cout << "$ " << flush;
+		getline(cin, input);
+		// char *in = fgets(input, MAXLINE, stdin); // Taking input one line at a time
+		//Checking for EOF
+		if (cin.eof()) {
+			//if (in == NULL){
+			if (DEBUG) printf("jash: EOF found. Program will exit.\n");
+			else printf("\n");
+			break ;
+		}
+		if (input == "history") {
+			for (int i = 0; i < history.size(); i++) cout << history[i] << endl;
+		}
+		else if (input == "_" && history.size()) input = history.at(history.size() - 1) ;
+		else {
+			if (history.size() && history.at(history.size()-1) != input) history.push_back(input);
+			else if (history.size() == 0) history.push_back(input);
+		}
+		// Calling the tokenizer function on the input line    
+		vector<string> tokens = tokenize(input);	
+		// Executing the command parsed by the tokenizer
+		if (input != "history") execute_command(tokens) ; 
+		
+	}
+	
+	/* Kill all Children Processes and Quit Parent Process */
+	return 0 ;
+}
+
+
+// Tokenizes a string into vector of strings (tokens)
+vector<string> tokenize(string command) {
+	vector<string> ans;
+	bool double_quotes = false;
+	string s = "";
+	for (int i = 0; i < command.size(); i++) {
+		if (command[i] == '\\') {
+			if (i < command.size()-1) s += command[i+1];
+			i++;
+		}
+		else if ((command[i] == ' ' || command[i] == '\n' || command[i] == '\t') && !(double_quotes)) {
+			if (s.size()) ans.push_back(s);
+			s = "";
+		}
+		else if (command[i] == '"') {
+			double_quotes = !double_quotes;
+		}
+		else {
+			s += command[i];
+		}
+	}
+	if (s.size()) ans.push_back(s);
+	return ans;
+}
+
+// executes the commands for jash script
+int execute_command(vector<string> tokens) {
+	int error;
+	FILE* fp;
+	char * line = NULL;
+  	size_t len = 0;
+   	ssize_t read;
+   	char* home = getenv("HOME");
+   	string homes(home);
+
+
+	/**
+	 * For input file redirection
+	 * infile = name of the file to take input from
+	 * outfile = name of the file to redirect output to
+	 * append_output = whether to append the output to the output file
+	 */
+	string infile = "", outfile = "";
+	bool append_output = false;
+	if (tokens[0] != "parallel" && tokens[0] != "sequential") {
+		for (int i = 0; i < tokens.size(); i++) {
+			if (tokens[i] == "<") {
+				infile = tokens[i+1];
+				i++;
+			}
+			else if(tokens[i] == ">") {
+				outfile = tokens[i+1];
+				append_output = false;
+			}
+			else if(tokens[i] == ">>") {
+				outfile = tokens[i+1];
+				append_output = true;
+			}
+		}
+
+		vector<string> tokens_temp = tokens;
+		tokens.clear();
+		for (int i = 0;i < tokens_temp.size(); i++) {
+			if (tokens_temp[i] == "<" || tokens_temp[i] == ">" || tokens_temp[i] == ">>") {
+				break;
+			}
+			tokens.push_back(tokens_temp[i]);
+		}
+	}
+	
+	
+	
+	/**
+	 * Replaces ~ with home directory location
+	 */
+   	for (int i = 0; i< tokens.size(); i++){
+   		if (tokens[i] == "~"){
+   			tokens[i] = homes;
+   		}
+   		else if (tokens[i][0] == '~' && tokens[i][1] == '/'){
+   			tokens[i] = homes + tokens[i].substr(1);
+   		}
+   	}
+	
+
+	/** 
+	 * Takes the tokens from the parser and based on the type of command 
+	 * and proceeds to perform the necessary actions. 
+	 * Returns 0 on success, -1 on failure. 
+	 */
+	if (tokens.size() == 0) {
+	 	return -1 ; 				// Null Command
+	 }
+	if (tokens[0] == "") {
+		return 0 ;					// Empty Command
+	} else if (tokens[0] == "exit") {
+		exit(0);
+		/* Quit the running process */
+		return 0 ;
+	} else if (tokens[0] == "cd") {
+		
+		if (tokens.size() < 2){
+			tokens.push_back(homes);
+		}
+		/* Change Directory, or print error on failure */
+		error = chdir(tokens[1].c_str());
+		//printf ("%d\n", error);
+		if (error != 0){
+			perror("cd");
+			return -1;
+		}
+		return 0 ;
+	} else if (tokens[0] == "parallel") {
+		/* Analyze the command to get the jobs */
+		/* Run jobs in parallel, or print error on failure */
+		vector<vector<string> > v = split(tokens);
+		for (int i = 0; i < v.size(); i++) {
+			int pid = fork();
+			if (pid == 0) {
+				exit(execute_command(v[i]));
+			}
+			else if (pid == -1) return -1;
+		}
+		for (int i = 0; i < v.size(); i++) waitpid(0, NULL, 0);
+		return 0 ;
+	} else if (tokens[0] == "sequential") {
+		/* Analyze the command to get the jobs */
+		/* Run jobs sequentially, print error on failure */
+		/* Stop on failure or if all jobs are completed */
+		vector<vector<string> > v = split(tokens);
+		for (int i = 0; i < v.size(); i++) {
+			int code = execute_command(v[i]);
+			if (code == -1) return -1;
+		}
+		return 0;					// Return value accordingly
+	} else if (tokens[0] == "sequential_or") {
+		/* Analyze the command to get the jobs */
+		/* Run jobs sequentially, print error on failure */
+		/* Stop on success or if all jobs are completed */
+		vector<vector<string> > v = split(tokens);
+		for (int i = 0; i < v.size(); i++) {
+			int code = execute_command(v[i]);
+			if (code == 0) return 0;
+		}
+		return -1;					// Return value accordingly
+	} else {
+		/* Either file is to be executed or batch file to be run */
+		/* Child process creation (needed for both cases) */
+		int pid = fork() ;
+		if (pid == -1) return -1;
+		if (pid == 0) {
+			if (tokens[0] == "run") {
+				/* Locate file and run commands */
+				/* May require recursive call to execute_command */
+				/* Exit child with or without error */
+				fp = fopen(tokens[1].c_str(), "r");
+				if (fp != NULL){
+					while ((read = getline(&line, &len, fp)) != -1) {
+			           tokens = tokenize(line);
+			           execute_command(tokens);
+			       }
+				}
+				else{
+					perror("Error");
+					fclose(fp);
+					exit(-1);
+				}
+				fclose(fp);
+				exit (0) ;
+			}
+			else {
+				
+				// Uses execvp function to find the file from PATH variables execute it with given arguments
+				const char **argv = new const char* [tokens.size()+2];   // extra room for program name and sentinel
+			    for (int j = 0;  j < tokens.size()+1;  ++j)     // copy args
+			            argv [j] = tokens[j] .c_str();
+
+			    argv [tokens.size()] = NULL;
+
+				error = execvp(tokens[0].c_str(), (char**) argv);
+
+				if (error == -1){
+					cerr<<"Command "<<tokens[0]<<" not found"<<endl;
+					deallocated(argv, tokens.size() + 2);
+					exit(-1);
+				}
+				deallocated(argv, tokens.size() + 2);
+				exit(0);
+			}
+		}
+		else {
+
+			/* Parent Process */
+			/* Wait for child process to complete */
+			int status;
+			waitpid(pid, &status, 0);
+			if (status == 0){
+				return 0;
+			}
+			return -1;
+		}
+	}
+	return 1 ;
+}
+
